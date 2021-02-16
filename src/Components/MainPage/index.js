a334f1a550b1f635c24871d11efbbb77c97aac8d
Cooper Terrones <cooper.terrones@outlook.com>
2021-02-15 20:58:46 -0700

    start of nav layout and links

--- src/Components/MainPage/index.js
+++ src/Components/MainPage/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import Nav from "../Nav/index";
 import logo from "../../Assets/logo.png";
 import "./MainPage.scss";
+import { Link } from "react-router-dom";
 
 const MainPage = () => {
   return (
-    <div>
-      <img className="coop-logo" src={logo} height="100px" width="100px" />
+    <div className="main-page-wrapper">
+      <Link to="/">
+        <img className="coop-logo" src={logo} height="100px" width="100px" />
+      </Link>
       <Nav />
     </div>
   );
--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,6 +1,21 @@
 import React from "react";
+import "./Nav.scss";
+import { Link } from "react-router-dom";
+
 const Nav = () => {
-  return <div className="nav-bar"></div>;
+  return (
+    <div className="nav-bar">
+      <Link className="link" to="/skills">
+        <h2 className="nav-element">skills</h2>
+      </Link>
+      <Link className="link" to="/projects">
+        <h2 className="nav-element">projects</h2>
+      </Link>
+      <Link className="link" to="/contact">
+        <h2 className="nav-element">contact</h2>
+      </Link>
+    </div>
+  );
 };
 
 export default Nav;
