270135270c053d5f15abb2085920f91850ec15db
Cooper Terrones <cooper.terrones@outlook.com>
2021-03-09 16:50:14 -0700

    Background art layout test

---
+++ src/Components/BackgroundArt/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./BackgroundArt.scss";
+
+const BackgroundArt = () => {
+  return (
+    <div className="background-container">
+      <p>layout test</p>
+    </div>
+  );
+};
+
+export default BackgroundArt;
