9825d465af7621d4444c2cc1bf2dbec4e98c45c2
Cooper Terrones <cooper.terrones@outlook.com>
2021-03-26 13:21:03 -0600

    Cursor Styling updated

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,45 +1,15 @@
 import React from "react";
 import "./Nav.scss";
-import {
-  Link,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-  scroller,
-} from "react-scroll";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <div className="nav-bar">
-      <Link
-        to="skills"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="scroll-link"
-        activeClass="active-scroll-link"
-        delay={1000}>
-        skills
+      <Link to="/skills" className="scroll-link">
+        placeholderLogo
         <div className="active-dot-1 hidden"></div>
       </Link>
-      <Link
-        to="projects"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="scroll-link"
-        activeClass="active-scroll-link">
-        projects
-        <div className="active-dot-2 hidden"></div>
-      </Link>
-      <Link
-        to="contact"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="scroll-link"
-        activeClass="active-scroll-link">
+      <Link to="/contact" className="scroll-link">
         contact
         <div className="active-dot-3 hidden"></div>
       </Link>
