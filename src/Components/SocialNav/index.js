cb9b8cdd5f05e6af44031e0217495376d8a87517
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-01 21:29:31 -0600

    Social nav updates with resume linking to notion

--- src/Components/SocialNav/index.js
+++ src/Components/SocialNav/index.js
@@ -12,6 +12,11 @@ const SocialNav = () => {
         href="https://www.linkedin.com/in/cooper-terrones-39b508185/">
         linked
       </a>
+      <a
+        className="social-link"
+        href="https://www.notion.so/Cooper-Terrones-f35ea597b7b945af861d72fe49e9a0e1">
+        resume
+      </a>
     </div>
   );
 };
