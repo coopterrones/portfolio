0bae2718590784593df4882bde7c0fce780f52bf
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-01 21:28:57 -0600

    Footer nav updates

--- src/Components/FooterNav/index.js
+++ src/Components/FooterNav/index.js
@@ -5,10 +5,9 @@ import "./FooterNav.scss";
 const FooterNav = () => {
   return (
     <div className="footer-nav-container">
-      <Link to="https://github.com/coopterrones">work.</Link>
+      <Link to="/work">work.</Link>
+      <Link to="/film">film.</Link>
       <Link to="/sandbox">sandbox.</Link>
-      <Link to="/film">film photo.</Link>
-      <Link to="/current-projects">currently working on.</Link>
     </div>
   );
 };
