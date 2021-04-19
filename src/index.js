50641268ae1bcc73c83caa29238cee5cddd43b44
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-19 11:39:03 -0600

    Favicon update added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./Components/App/index.js";
 import { BrowserRouter } from "react-router-dom";
+import Favicon from "react-favicon";
+import coopLogo from "./Assets/cooplogobg.png";
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
+    <Favicon url={coopLogo} />
   </BrowserRouter>,
   document.getElementById("root")
 );
