e1fe6d64e6d866752c51082fe5ca6844af6d8d00
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-19 11:39:18 -0600

    Store updates for sandbox

--- src/store.js
+++ src/store.js
@@ -6,4 +6,22 @@ export const useStore = create((set) => ({
     { name: "Poppn Co.", description: "asdf", image: "placeholder" },
     { name: "Highland Style", description: "asdf", image: "placeholder" },
   ],
+  sandbox: [
+    { name: "Cappua", description: "asdf", link: "", image: "placeholder" },
+    {
+      name: "Intention Timer",
+      description: "asdf",
+      link: "",
+      image: "placeholder",
+    },
+    { name: "Slapjack", description: "asdf", link: "", image: "placeholder" },
+    { name: "Overlook", description: "asdf", link: "", image: "placeholder" },
+    {
+      name: "Rancid Tomatillos",
+      description: "asdf",
+      link: "",
+      image: "placeholder",
+    },
+    { name: "Favo", description: "asdf", link: "", image: "placeholder" },
+  ],
 }));
