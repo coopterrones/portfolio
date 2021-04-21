6e7ef86313e9bacbe6fe2d063b915bb50b71271f
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-20 22:31:30 -0600

    Sandbox items updated with grid

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -4,8 +4,13 @@ import Work from "../Work/index";
 import { Route } from "react-router";
 import Grid from "../Grid";
 import WorkDetails from "../WorkDetails";
+import { useStore } from "../../store";
 
 function App() {
+  const store = useStore((state) => state);
+
+  const sandboxItems = store.sandbox;
+
   return (
     <div className="App">
       <Route exact path="/">
@@ -15,9 +20,18 @@ function App() {
         <Work />
       </Route>
       <Route exact path="/sandbox">
-        <Grid />
+        <Grid
+          header="This is my sandbox. A safe space to experiment and flex my imagination."
+          subheader="A place
+          to let my mind wander and grow."
+          items={sandboxItems}
+        />
       </Route>
-      <Route exact path="/film"></Route>
+      <Route exact path="/film">
+        <Grid
+          header="The beauty of film lies in the finite. The comfort in the imperfection."
+          subheader="Film captures the emotion of which a sensor could never recreate."></Grid>
+      </Route>
       <Route exact path="/current-projects"></Route>
       <Route exact path="/work-canoe-club">
         <WorkDetails name="Canoe Club" />
--- src/Components/Grid/index.js
+++ src/Components/Grid/index.js
@@ -4,10 +4,12 @@ import SocialNav from "../SocialNav";
 import FooterNav from "../FooterNav";
 import "./Grid.scss";
 
-const Grid = () => {
+const Grid = ({ header, subheader, items }) => {
   return (
     <section className="grid-container">
       <Nav />
+      <h2 className="header-description">{header}</h2>
+      <h3 className="sub-header-description">{subheader}</h3>
       <div className="grid-items-container"></div>
       <SocialNav></SocialNav>
       <FooterNav></FooterNav>
