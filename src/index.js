518e06244e6432682ac46318745a85e9f64afb0e
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-21 09:30:47 -0600

    Update basename -- test for deployment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Favicon from "react-favicon";
 import coopLogo from "./Assets/cooplogobg.png";
 
 ReactDOM.render(
-  <BrowserRouter basename="https://portfolio-eippl7shb-coopterrones.vercel.app">
+  <BrowserRouter basename="/portfolio">
     <App />
     <Favicon url={coopLogo} />
   </BrowserRouter>,
