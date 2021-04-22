110d8fdeef920327e002fd2a2bf5d22c1b65744c
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-22 15:34:27 -0600

    Basename test for deployment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./Components/App/index.js";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="https://www.coop.design">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
