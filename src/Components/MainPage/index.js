5ac29a4aace211c06baf23fecd8ef08baf310eb6
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-22 18:37:18 -0600

    Clean up files

--- src/Components/MainPage/index.js
+++ src/Components/MainPage/index.js
@@ -3,29 +3,11 @@ import Nav from "../Nav/index";
 import FooterNav from "../FooterNav/index";
 import "./MainPage.scss";
 import BackgroundArt from "../BackgroundArt/index";
-import {
-  Link,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-  scroller,
-} from "react-scroll";
 import SocialNav from "../SocialNav/index";
 
 const MainPage = () => {
   return (
     <section name="main" className="main-page-wrapper">
-      {/* <Link
-        to="main"
-        spy={true}
-        smooth={true}
-        duration={500}
-        className="logo-scroll-link"
-        activeClass="active-scroll-link"
-        delay={1000}>
-        <img className="coop-logo" src={logo} height="150px" width="150px" />
-      </Link> */}
       <Nav />
       <BackgroundArt />
       <SocialNav></SocialNav>
