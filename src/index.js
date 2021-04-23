cc435f251b53de6340021317d4ee4839f301fd78
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-22 22:36:21 -0600

    Basename update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./Components/App/index.js";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/portfolio">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
